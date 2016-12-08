62f73fa9019cd6601a1ca82ed62a1bd019571d79
Amber Coding <a.mariecarter@yahoo.com>
2016-12-08 13:13:28 -0500

    tic tac toe add

---
+++ tic-tac-toe/scripts.js
@@ -0,0 +1,7 @@
+// user should be able to click a box and marks hows up
+// put onclick in the first squuare
+// when the user clicks call function that puts x in the box
+// MILESTONE
+function markSquare(square){
+	console.log(square.id);
+}
